0d57ecfa0ed1a8ce649acd495c05db99f254dac7
kmkim <kmkimplats@gmail.com>
2025-09-18 15:08:38 +0900

    [ADD] Request Interceptor 추가

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { GlobalExceptionFilter } from "./exception/GlobalExcetpion";
+import { RequestLoggingInterceptor } from "./interceptor/RequestLoggingInterceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new RequestLoggingInterceptor());
 
   await app.listen(process.env.PORT ?? 4040);
 }
---
+++ src/interceptor/RequestLoggingInterceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+
+@Injectable()
+export class RequestLoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const req = context.switchToHttp().getRequest();
+    const method = req.method;
+    const url = req.url;
+
+    console.log(`[Request] ${method} ${url} - start`);
+
+    // 응답 후 로직 없음, 요청 처리만 로그
+    return next.handle();
+  }
+}
